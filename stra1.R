dae5a4c60ef0cf35171438ea7042d7ccd43ca21e
Alex Lee <lu.lee05@gmail.com>
2012-07-06 19:14:29 +0800

    h

--- stra1.R
+++ stra1.R
@@ -15,9 +15,10 @@ require(FactorAnalytics)
 load(file.path(this.dir, "sample.500ms.rda"))
 
 mkt <- sample.500ms
-mkt <- mkt["T09:15/T15:14"]
+mkt <- mkt["T09:14/T15:15"]
 mkt <- to.period(mkt, "seconds", k = 1)
 mkt <- to.minutes(mkt)
+
 
 strategy.name <- "ma_adx"
 suppressWarnings(rm(list=paste("order_book", strategy.name, sep="."), pos=.strategy))
@@ -32,13 +33,15 @@ account.name <- strategy.name
 
 fastMA <- 2 
 slowMA <- 12
+qty <- 100
+txnfee <- -20
 
-currency('RMB')
+currency('USD')
 print(ls_currencies())
-stock(contract, currency='RMB', multiplier=1)
-print(ls_stocks())
+stock(contract, currency='USD', multiplier=1)
+# print(ls_stocks())
 
-start.time <- '2011-10-10 10:11:20'
+start.time <- '2012-03-14 09:14:59'
 initial.equity <- 1000000
 
 IF2016 <- mkt
@@ -54,16 +57,13 @@ strat <- add.indicator(strat, label="slow.EMA", name='EMA', arguments=list(x=quo
 strat <- add.indicator(strat, label="fast.EMA", name='EMA', arguments=list(x=quote(Cl(mktdata)), n=2))
 strat <- add.indicator(strat, label="adx", name='ADX', arguments=list(HLC=quote(HLC(mktdata))))
 
-# mkt <- applyIndicators(strat, mkt)
-# print(head(mkt, n=30))
-
 # signals
 strat <- add.signal(strat, name="sigCrossover", arguments = list(columns=c("fast.EMA", "slow.EMA"), relationship="gte"), label="fast.cross.above.slow")
 strat <- add.signal(strat, name="sigCrossover", arguments = list(columns=c("fast.EMA", "slow.EMA"), relationship="lt"), label="fast.cross.below.slow")
 strat <- add.signal(strat, name="sigThreshold", arguments = list(column="ADX", relationship="gte", threshold=28), label="adx.gte.threshold")
 strat <- add.signal(strat, name="sigThreshold", arguments = list(column="ADX", relationship="lt", threshold=28), label="adx.lt.threshold") 
 
-sigLongBuy <- function(label, data = mktdata) {
+sigBuyLong <- function(label, data = mktdata) {
   signal <- xts(rep(FALSE, nrow(data)), order.by=index(data))
   trend <- sigThreshold(label="", data=mktdata, column="adx.ADX", threshold=28, relationship="gte")
   crossup <- sigCrossover(label="", data=mktdata, columns=c("fast.EMA", "slow.EMA"), relationship="gte")
@@ -71,7 +71,17 @@ sigLongBuy <- function(label, data = mktdata) {
   colnames(signal) <- label
   return(signal)
 }
-strat <- add.signal(strat, label="long.buy", name="sigLongBuy", arguments=list(data=quote(mktdata)))
+strat <- add.signal(strat, label="buy.long", name="sigBuyLong", arguments=list(data=quote(mktdata)))
+
+sigSellShort <- function(label, data = mktdata) {
+  signal <- xts(rep(FALSE, nrow(data)), order.by=index(data))
+  trend <- sigThreshold(label="", data=mktdata, column="adx.ADX", threshold=28, relationship="lt")
+  crossup <- sigCrossover(label="", data=mktdata, columns=c("fast.EMA", "slow.EMA"), relationship="lt")
+  signal <- trend & crossup
+  colnames(signal) <- label
+  return(signal)
+}
+strat <- add.signal(strat, label="sell.short", name="sigSellShort", arguments=list(data=quote(mktdata)))
 
 sigMarketClose <- function(label, data = mktdata, period) {
   signal <- xts(rep(FALSE, nrow(data)), order.by=index(data))
@@ -81,21 +91,25 @@ sigMarketClose <- function(label, data = mktdata, period) {
 }
 strat <- add.signal(strat, name="sigMarketClose", arguments = list(data=quote(mktdata), period="T15:00/T15:15"), label="market.close") 
 
-# mkt <- applySignals(strat, mkt)
+# mkt <- applySignals(strat, mktdata=applyIndicators(strat, mktdata=IF2016)) #for debugging
 # print(head(mkt, n=300))
 
 # rules
-strat <- add.rule(strat, name="ruleSignal", arguments = list(sigcol="long.buy", sigval=TRUE, orderqty=100, ordertype='market', orderside='long'), type='enter')
-strat <- add.rule(strat, name="ruleSignal", arguments = list(sigcol="fast.lt.slow", sigval=TRUE, orderqty="all", ordertype='market', orderside='long'), type='exit')
+# buy long
+strat <- add.rule(strat, name="ruleSignal", arguments = list(sigcol="buy.long", sigval=TRUE, orderqty=100, ordertype='market', orderside='long', pricemethod='market', replace=FALSE, TxnFees=-2), type='enter', path.dep=TRUE)
+strat <- add.rule(strat, name="ruleSignal", arguments = list(sigcol="fast.cross.below.slow", sigval=TRUE, orderqty="all", ordertype='market', orderside='long', pricemethod='market', replace=FALSE, TxnFees=-2), type='exit', path.dep=TRUE)
+# sell short
+strat <- add.rule(strat, name="ruleSignal", arguments = list(sigcol="sell.short", sigval=TRUE, orderqty=-100, ordertype='market', orderside='short', pricemethod='market', replace=FALSE, TxnFees=-2), type='enter', path.dep=TRUE)
+strat <- add.rule(strat, name="ruleSignal", arguments = list(sigcol="fast.cross.above.slow", sigval=TRUE, orderqty="all", ordertype='market', orderside='short', pricemethod='market', replace=FALSE, TxnFees=-2), type='exit', path.dep=TRUE)
+
 strat <- add.rule(strat, name="ruleSignal", arguments = list(sigcol="market.close", sigval=TRUE, orderqty="all", ordertype='market', orderside='long'), type='risk')
 strat <- add.rule(strat, name="ruleSignal", arguments = list(sigcol="market.close", sigval=TRUE, orderqty="all", ordertype='market', orderside='short'), type='risk')
 
-print(strat)
+out <- applyStrategy(strat, portfolios=portfolio.name)
 
-out <- try(applyStrategy(strat , portfolios=portfolio.name, parameters=list()))
-updatePortf(Portfolio=portfolio.name)
+updatePortf(Portfolio=portfolio.name, Dates="2012-03-14T09:14:59::2012-03-14T15:15:00")
 
-#chart.Posn(Portfolio=portfolio.name)
+chart.Posn(Portfolio=portfolio.name)
 # plot(add_MACD(fast=fastMA, slow=slowMA, signal=signalMA,maType="EMA"))
 
 #look at the order book
