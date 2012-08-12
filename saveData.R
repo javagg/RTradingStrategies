f3e8a5fd3abfa2aef7ca653a335c2bf1d097587a
Alex Lee <lu.lee05@gmail.com>
2012-08-12 22:09:57 +0800

    all

--- saveData.R
+++ saveData.R
@@ -1,6 +1,6 @@
 symbols <- c(paste("IF110", 4:9, sep=""), paste("IF111", 0:2, sep=""), paste("IF120", 1:6, sep=""))
-# symbols <- c("IF1104")
-
+symbols <- c("IF1101")
+symbols <- c("IF1101", "IF1102", "IF1103", "IF1011", "IF1012")
 fields <- c("high", "low", "close", "bid", "ask", "volume")
 getSymbols(symbols, src="MySQL", user="test", password="123456", dbname="historydata", db.fields=c("date", "open", "high", "low", "close", "bid", "ask", "volume", "adjusted"), field.names=c("open", "high", "low", "close", "bid", "ask", "volume", "adjusted"))
 
